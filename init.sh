b5f3855799ddb82a728fbfdeb3c99b973fbbe7e0
Hitoshi kuroyanagi <h-kuro@minerva.co.jp>
2015-08-06 16:59:30 +0900

    init.shを整理

--- init.sh
+++ init.sh
@@ -12,22 +12,25 @@ cd $DOTFILES
 
 git submodule update --init
 
-rm -rf $HOME/.bashrc $HOME/.gemrc $HOME/.gitconfig $HOME/.profile $HOME/.vimrc $HOME/.vim $HOME/.conkyrc $HOME/scripts  $HOME/.fonts
+rm -rf $HOME/.bashrc $HOME/.gemrc $HOME/.gitconfig $HOME/.profile $HOME/.vimrc $HOME/.vim $HOME/scripts  $HOME/.fonts
 rm -rf $HOME/.zshrc $HOME/.zlogin $HOME/.zshenv
 
 #ln -s $DOTFILES/dot.bashrc $HOME/.bashrc
-ln -s $DOTFILES/dot.zshrc $HOME/.zshrc
-ln -s $DOTFILES/dot.zlogin $HOME/.zlogin
-ln -s $DOTFILES/dot.zshenv $HOME/.zshenv
+#ln -s $DOTFILES/dot.zshrc $HOME/.zshrc
+#ln -s $DOTFILES/dot.zlogin $HOME/.zlogin
+#ln -s $DOTFILES/dot.zshenv $HOME/.zshenv
 ln -s $DOTFILES/dot.gemrc $HOME/.gemrc
 ln -s $DOTFILES/dot.gitconfig $HOME/.gitconfig
 ln -s $DOTFILES/dot.profile $HOME/.profile
+
+# Ruby,PHP Enviroment
+git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git  $HOME/.rbenv/plugins/ruby-build
+#git clone https://github.com/php-build/php-build.git
+
+# Vim Setting
+cd $DOTFILES
 ln -s $DOTFILES/dot.vimrc $HOME/.vimrc
 ln -s $DOTFILES/vimfiles $HOME/.vim
-#ln -s $DOTFILES/dot.conkyrc $HOME/.conkyrc
-#ln -s $DOTFILES/conkyfiles/scripts $HOME/scripts
-#ln -s $DOTFILES/conkyfiles/.fonts $HOME/.fonts
 ln -s $DOTFILES/syntax $DOTFILES/vimfiles/syntax
-#touch $HOME/weather
-
-#vim -c ':BundleInstall!'
+vim -c ':BundleInstall!'
